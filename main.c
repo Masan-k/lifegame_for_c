3f40f24d16a3a9420ee638c20e78bfceba1a25ea
Masan-k <uronti@hotmail.co.jp>
2023-01-21 22:29:07 +0900

    add input

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 int main(void)
 {
-    int year = 2016; // うるう年
-    if(year % 4 == 0) {
-        printf("%d年はうるう年です！\n", year);
-    }
-    return 0;
+  int year;
+
+  printf("year = ");
+  scanf("%d", &year);
+
+  if(year % 4 == 0) {
+    printf("%d年はうるう年です！\n", year);
+  }else{
+    printf("%d年はうるう年ではありません！\n", year);
+  }
+  return 0;
 }
