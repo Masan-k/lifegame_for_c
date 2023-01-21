1cd5407fbd5ddc259048e9ccf7c41dfccdfa0200
Masan-k <uronti@hotmail.co.jp>
2023-01-22 02:17:24 +0900

    show time

--- main.c
+++ main.c
@@ -2,9 +2,7 @@
 #include <time.h>
 int main(void)
 {
- //const int ROW_MAX = 10;
- //const int COL_MAX = 10;
- int cell[10][10] =\
+  int cell[10][10] =\
       {{1,0,0,0,0,0,0,0,0,0}\
       ,{0,0,0,0,0,0,0,0,0,0}\
       ,{0,0,0,0,0,0,0,0,0,0}\
@@ -16,12 +14,27 @@ int main(void)
       ,{0,0,0,0,0,0,0,0,0,0}\
       ,{0,0,0,0,0,0,0,0,9,9}};
 
- int col,row;
- for(row=0; row<10; row++){
-   for(col=0; col<10; col++){
-     printf("%d",cell[row][col]);
-   }
-   printf("\n");
- }
- return 0;
+  clock_t start = clock();
+  int dispSec = -1;
+  int col,row;
+  for (;;){
+    clock_t end = clock();
+    int sec = (double)(end - start) / CLOCKS_PER_SEC;
+    if(sec != dispSec){
+      dispSec = sec;
+      printf("\033[2J");   /* 画面をクリア */
+      printf("\033[1;1H"); /* カーソルを移動 */
+      //puts("\x1b[2J");
+      printf("-------------------\n");
+      printf("経過時間 : %d\n",sec);
+      printf("-------------------\n");
+      for(row=0; row<10; row++){
+        for(col=0; col<10; col++){
+          printf("%d",cell[row][col]);
+        }
+      printf("\n");
+      }
+    }
+  }
+  return 0;
 }
