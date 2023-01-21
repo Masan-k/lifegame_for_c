18a655b8f8a25d365cf758e887f9b556bee9d782
Masan-k <uronti@hotmail.co.jp>
2023-01-21 23:08:01 +0900

    add inputTest cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 using namespace std;
 int main() {
-  if (num == 0) {
-    cout << "0";
+
+  int i;
+  cout << "数値を入力してください";
+  cout << " i = ";
+  cin >> i;
+  cout << "\n入力された数値は " << i <<"です\n";
+
+  float f;
+  cout << "数値を入力してください f = ";
+  cin >> f;
+  cout << "\n入力された数値は " << f <<"です\n";
+
+  if (i == 0) {
+    cout << "iは0です";
   }
-  else if (num == 1) {
-    cout << "1"; // 1が表示される
+  else if (i == 1) {
+    cout << "iは1です";
   }
   else {
-    cout << "01以外";
+    cout << "iは「0」または「1」以外です";
   }
   return 0;
 }
