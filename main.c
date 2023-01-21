05ee78ee359139446b0a9a59f3701ae1fde14eaf
Masan-k <uronti@hotmail.co.jp>
2023-01-21 22:05:26 +0900

    add ifTest

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 int main(void)
 {
- printf("Hello World !!\n");
- return 0;
+    int year = 2016; // うるう年
+    if(year % 4 == 0) {
+        printf("%d年はうるう年です！\n", year);
+    }
+    return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,4 +1,14 @@
-#include <cstdio>
+#include <iostream>
+using namespace std;
 int main() {
-  printf("hello\n");
+  if (num == 0) {
+    cout << "0";
+  }
+  else if (num == 1) {
+    cout << "1"; // 1が表示される
+  }
+  else {
+    cout << "01以外";
+  }
+  return 0;
 }
