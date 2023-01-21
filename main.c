1ec37a577493102a09fb726d96abe4c9d8a81b74
Masan-k <uronti@hotmail.co.jp>
2023-01-21 23:56:47 +0900

    add arrayTest

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 int main(void)
 {
-  int count;
-  for (count = 1; count <= 3; count = count + 1){
-    printf("%d",count);
-    printf(" こんにちは\n");
+  int i;
+  int count[3] = {0,3,5};
+
+  for (i = 0; i <= 2; i = i+1){
+     printf("%d:Hello\n", count[i]);
   }
   return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
+#include <array>
 //using namespace std;
-
 int main() {
-  for (int i = 0; i < 3; i++) {
-    std::cout <<  i;
-    std::cout <<  ":Hello \n";
+  std::array<int, 10> arr{1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
+
+  if (!arr.empty()) {
+    std::cout << "array size: " << arr.size() << std::endl;
   }
+
+  std::cout << "-------------------------" << std::endl;
+  for (int i = 0; i < arr.size(); i++) {
+      std::cout << arr[i];
+      if(i < arr.size()-1){
+        std::cout << ",";
+      }
+  }
+  std::cout << std::endl << "-------------------------" << std::endl;
+  std::cout << "first element: " << arr.front() << std::endl; // 1
+  std::cout << "last element: " << arr.back() << std::endl;   // 55
   return 0;
 }
