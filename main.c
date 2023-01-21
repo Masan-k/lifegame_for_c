9f80340e892574351b5504766c52346a1726ba98
Masan-k <uronti@hotmail.co.jp>
2023-01-22 01:48:33 +0900

    add timeTest

--- main.c
+++ main.c
@@ -1,6 +1,31 @@
+/*
+ * Author: Hiroyuki Chishiro
+ * License: 2-Clause BSD
+ */
 #include <stdio.h>
-int main(void)
-{
- printf("Hello World !!\n");
- return 0;
+#include <time.h>
+ 
+int main(void) {
+  double total_sec = 10; 
+  clock_t start = clock();
+  int dispSec = -1;
+  printf("------------\n");
+  printf("カウント開始\n");
+  printf("------------\n");
+  for (;;) {
+    clock_t end = clock();
+    int sec = (double)(end - start) / CLOCKS_PER_SEC;
+    if(sec != dispSec){
+      printf("%d\n",sec);
+      dispSec = sec;
+    }
+    
+    if (sec >= total_sec) {
+      break;
+    }
+  }
+
+  printf("カウント終了（%f秒カウントしました。）\n", total_sec);
+
+  return 0;
 }
