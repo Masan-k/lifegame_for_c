a652e10d4801803aa788a2e859a8e6b914f4ded4
Masan-k <uronti@hotmail.co.jp>
2023-01-23 01:24:48 +0900

    c ok

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 int main(void)
 {
- printf("Hello World !!\n");
- return 0;
+  int cell[2][2] = {{0,1}\
+                  ,{2,3}};
+ 
+  char str[256] = "□";
+  char str2[256] = "■";
+
+  printf("strlen：□：%ld\n",strlen(str));
+  printf("strlen：■：%ld\n",strlen(str2));
+
+  int i,j;
+  for(i=0; i<2; i++){
+    char str3[256] = "";
+    for(j=0; j<2; j++){
+      if(cell[i][j] == 0){
+        strcat(str3,"□");
+      }else{
+        strcat(str3,"■");
+      }
+    }
+    printf("%s\n",str3);
+  }
+  return 0;
 }
